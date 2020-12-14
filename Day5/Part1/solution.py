043aa04bc164d1c7fe278628997e5c542323d4e7
German Godoy <germangodoygutierrez@gmail.com>
2020-12-14 14:07:43 +0000

    l

--- Day5/Part1/solution.py
+++ Day5/Part1/solution.py
@@ -1,42 +1,50 @@
 import sys
-import numpy as np
 
+#Open the file ans split it by lines (each line is a boarding pass)
 input_txt = open("input.txt", "r")
 input = input_txt.read().splitlines()
 input_txt.close()
 
-sites = np.zeros((128, 8))
+#BinString is the place where bin numbers will be stored
+bin_string = ''
 
-string = ''
-
+#Define the var where the highest id is going to be stored
 high_id = 0
 
+#MAIN LOOP
 for i in range(len(input)):
+    
+    #Define the actual id and restore the bin_string
     id = 0
-    string = ''
+    bin_string = ''
+
+    #Convert BFLR in binary
     for e in range(len(input[i])):
         if input[i][e] == 'B' or input[i][e] == 'R':
             char = '1'
         else:
             char = '0'
 
-        string += char
+        bin_string += char
+    
+    #We separate the bins for the row and for the collum
+    row = bin_string[0] + bin_string[1] + bin_string[2] + bin_string[3] + bin_string[4] + bin_string[5] + bin_string[6]
+    column = bin_string[7] + bin_string[8] + bin_string[9] 
 
-    row = string[0] + string[1] + string[2] + string[3] + string[4] + string[5] + string[6]
-    column = string[7] + string[8] + string[9] 
+    #Convert bin into int
     result_row = int(row, 2)
     result_column = int(column, 2)
 
+    #calculate ID
     id = result_row * 8 + result_column
 
+    #Change the highest id if actual is greater
     if id > high_id:
         high_id = id
 
-    sites[result_row][result_column] = 1
+#Print Results
+print("The highest id has ben: {}".format(high_id))
 
 
-print(column)
-print(row)
-print(result_column)
-print(result_row)
-print(high_id)
+#TERMINAL
+# The highest id has been: 826
--- Day5/Part2/solution.py
+++ Day5/Part2/solution.py
@@ -1,34 +1,47 @@
 import sys
 import numpy as np
 
+#Import the txt and split it by lines
 input_txt = open("input.txt", "r")
 input = input_txt.read().splitlines()
 input_txt.close()
 
-string = ''
+#Declare bin_string
+bin_string = ''
 
-id_array = np.zeros(127 * 8 + 8)
+#Declares an array with all the possible ids
+id_array = np.zeros(826)
 
+#MAIN LOOP
 for i in range(len(input)):
+
+    #Set string and actual id to empty
     id = 0
-    string = ''
+    bin_string = ''
+
+    #Convert BFLR into bin
     for e in range(len(input[i])):
         if input[i][e] == 'B' or input[i][e] == 'R':
             char = '1'
         else:
             char = '0'
 
-        string += char
+        bin_string += char
 
-    row = string[0] + string[1] + string[2] + string[3] + string[4] + string[5] + string[6]
-    column = string[7] + string[8] + string[9] 
+    #Store the actual row and column
+    row = bin_string[0] + bin_string[1] + bin_string[2] + bin_string[3] + bin_string[4] + bin_string[5] + bin_string[6]
+    column = bin_string[7] + bin_string[8] + bin_string[9]
+
+    #Convert bin to int  
     result_row = int(row, 2)
     result_column = int(column, 2)
 
+    #calculate actual id
     id = result_row * 8 + result_column
 
+    #Change the index id of the array to 1 (ocupied)
     id_array[id] = 1
 
-
-a=np.where(id_array == 1)
+#So the result will be the only index with a 0 (unocupied)
+a=np.where(id_array == 0)
 print(a)
